27ed2acfcb1ab432dd35ecfe2b2c7e781c803656
Utopia-015 <b174777@outlook.com>
2021-10-27 12:31:21 +0800

    correct names

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker network create --subnet=172.20.0.0/16 snet
+docker network create --subnet=172.20.0.0/16 snet
 
-docker run -d --name=master --hostname=master --network=snet --ip=172.20.1.0 --add-host=slave1:172.20.1.1 --add-host=slave2:172.20.1.2 --privileged ha:sp /usr/sbin/init
-docker run -d --name=slave1 --hostname=slave1 --network=snet --ip=172.20.1.1 --add-host=master:172.20.1.0 --add-host=slave2:172.20.1.2 --privileged ha:sp /usr/sbin/init
-docker run -d --name=slave2 --hostname=slave2 --network=snet --ip=172.20.1.2 --add-host=master:172.20.1.0 --add-host=slave1:172.20.1.1 --privileged ha:sp /usr/sbin/init
+docker run -d --name=master --hostname=master --network=snet --ip=172.20.1.0 --add-host=slave1:172.20.1.1 --add-host=slave2:172.20.1.2 --privileged hadoop:cluster /usr/sbin/init
+docker run -d --name=slave1 --hostname=slave1 --network=snet --ip=172.20.1.1 --add-host=master:172.20.1.0 --add-host=slave2:172.20.1.2 --privileged hadoop:cluster /usr/sbin/init
+docker run -d --name=slave2 --hostname=slave2 --network=snet --ip=172.20.1.2 --add-host=master:172.20.1.0 --add-host=slave1:172.20.1.1 --privileged hadoop:cluster /usr/sbin/init
